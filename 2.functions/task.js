692dd651ccaf16251ed81554f2b2eaf91fc5b9fb
gashpen <constantinent@mail.ru>
2023-01-09 22:37:15 +0300

    Homework2function_2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -4,26 +4,23 @@ function getArrayParams(...arr) {
   let sum = 0;
   let avg;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max){
+    if (arr[i] > max) {
       max = arr[i];
     }
-    if (arr[i] < min){
+    if (arr[i] < min) {
       min = arr[i];
     }
     sum += arr[i];
   }
   avg = sum / arr.length;
 
-  return { min: min, max: max, avg: Number(avg.toFixed(2))};
+  return { min: min, max: max, avg: Number(avg.toFixed(2)) };
 }
 
 function summElementsWorker(...arr) {
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
-    if(arr.length == 0){
-      return 0
-    }
   }
   return sum
 }
@@ -33,12 +30,15 @@ function differenceMaxMinWorker(...arr) {
   let max = -Infinity;
   let difference = 0;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max){
+    if (arr[i] > max) {
       max = arr[i];
     }
-    if (arr[i] < min){
+    if (arr[i] < min) {
       min = arr[i];
     }
+  }
+  if (arr.length == 0) {
+    return 0
   }
   difference = max - min;
   return difference
@@ -54,6 +54,9 @@ function differenceEvenOddWorker(...arr) {
       sumOddElement += arr[i]
     }
   }
+  if (arr.length == 0) {
+    return 0
+  }
   return sumEvenElement - sumOddElement
 }
 
@@ -61,23 +64,27 @@ function averageEvenElementsWorker(...arr) {
   let sumEvenElement = 0;
   let countEvenElement = 0;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 == 0){
+    if (arr[i] % 2 == 0) {
       sumEvenElement += arr[i];
       countEvenElement++
     }
+  }
+  if (arr.length == 0) {
+    return 0
   }
   return sumEvenElement / countEvenElement
 }
 
 function makeWork(arrOfArr, func) {
   let maxWorkerResult = - Infinity;
-  let sumFunc = 0;
   for (let i = 0; i < arrOfArr.length; i++) {
-    sumFunc = func(...arrOfArr[i]);
-  }
-  if(sumFunc > maxWorkerResult){
-    maxWorkerResult = sumFunc
+    let resultFunc = 0;
+    resultFunc = func(...arrOfArr[i]);
+    if (resultFunc > maxWorkerResult) {
+      maxWorkerResult = resultFunc
+    }
   }
   return maxWorkerResult
 }
 makeWork(arr, averageEvenElementsWorker)
+const arr = [[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [30, 41, 55, 96, 62]];
