6b3ca4d9e4b11d7894c84104ed64bbeef387e911
gashpen <constantinent@mail.ru>
2023-01-09 15:01:47 +0300

    Homework2function_1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -4,10 +4,12 @@ function getArrayParams(...arr) {
   let sum = 0;
   let avg;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max)
+    if (arr[i] > max){
       max = arr[i];
-    if (arr[i] < min)
+    }
+    if (arr[i] < min){
       min = arr[i];
+    }
     sum += arr[i];
   }
   avg = sum / arr.length;
@@ -15,31 +17,32 @@ function getArrayParams(...arr) {
   return { min: min, max: max, avg: Number(avg.toFixed(2))};
 }
 
-getArrayParams(-99, 99, 10)
-
 function summElementsWorker(...arr) {
   let sum = 0;
-  for (let i = 0; i < arr.length; i++){
-  sum += arr[i];
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    if(arr.length == 0){
+      return 0
+    }
   }
   return sum
 }
-summElementsWorker(1,2,3)
 
 function differenceMaxMinWorker(...arr) {
   let min = Infinity;
   let max = -Infinity;
   let difference = 0;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max)
+    if (arr[i] > max){
       max = arr[i];
-    if (arr[i] < min)
+    }
+    if (arr[i] < min){
       min = arr[i];
+    }
   }
   difference = max - min;
   return difference
 }
-differenceMaxMinWorker(10, 10, 11, 20, 10)
 
 function differenceEvenOddWorker(...arr) {
   let sumEvenElement = 0;
@@ -53,7 +56,6 @@ function differenceEvenOddWorker(...arr) {
   }
   return sumEvenElement - sumOddElement
 }
-differenceEvenOddWorker(94, 51, 57, 41, 47, 66, 58, 10, 38, 17)
 
 function averageEvenElementsWorker(...arr) {
   let sumEvenElement = 0;
@@ -66,10 +68,16 @@ function averageEvenElementsWorker(...arr) {
   }
   return sumEvenElement / countEvenElement
 }
-averageEvenElementsWorker(1, 2, 3, 4, 5, 6, 7, 8, 9)
 
-function makeWork (arrOfArr, func) {
+function makeWork(arrOfArr, func) {
   let maxWorkerResult = - Infinity;
-  for(let i = 0;i < arrOfArr.length; i++)
-    arrOfArr[i] = func(...i)
+  let sumFunc = 0;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    sumFunc = func(...arrOfArr[i]);
+  }
+  if(sumFunc > maxWorkerResult){
+    maxWorkerResult = sumFunc
+  }
+  return maxWorkerResult
 }
+makeWork(arr, averageEvenElementsWorker)
