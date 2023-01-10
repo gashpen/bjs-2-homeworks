baa8e348bab0a9222cfbe237b922f72436192e43
gashpen <constantinent@mail.ru>
2023-01-10 23:09:00 +0300

    homework3_

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,6 +1,10 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    if(arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index])){
+      return true
+    } else {
+      return false
+    }
+  }
 
 function getUsersNamesInAgeRange(users, gender) {
   
