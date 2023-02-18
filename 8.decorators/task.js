72a42098cd68e3a320d6c3dd2f0a1fe5272d8146
gashpen <constantinent@mail.ru>
2023-02-18 17:36:39 +0300

    Homework8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,50 @@
 //Задача № 1
-function cachingDecoratorNew(func) {
-  
-}
+    function cachingDecoratorNew(func) {
+        let cache = [];
+        
+        function wrapper(...args) {
+            const hash = md5(args); // получаем правильный хеш c помощью функции md5
+            let objectInCache = cache.find((item) => item === cache[hash]); // ищем элемент, хеш которого равен нашему хешу
+            if (objectInCache) { // если элемент найден
+                console.log("Из кэша: " + cache[hash]); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+                return "Из кэша: " + cache[hash];
+            }
+        
+            let result = func(...args); // в кеше результата нет — придётся считать
+            cache.push(hash) ; // добавляем элемент с правильной структурой
+            if (cache.length > 5) { 
+              cache = cache.shift()// если слишком много элементов в кеше, надо удалить самый старый (первый) 
+            }
+            console.log("Вычисляем: " + result);
+            return "Вычисляем: " + result;  
+        }
+        return wrapper;
+        }
+
+
+        
 
 //Задача № 2
+const sum = (...args) => args.reduce((acc, item) => acc + item, 0);
+
 function debounceDecoratorNew(func, delay) {
-  
+  let isThrottled = false;
+  let allCount = [];
+  let count = 0;
+  function wrapper(...args){
+    if(isThrottled){
+      return;
+    }
+    func(...args);
+    isThrottled = true;
+    setInterval(() => isThrottled = false, delay);
+    setTimeout(() => func(args), delay);
+    count++;
+    
+  }
+  wrapper.count = () => count;
+  return wrapper
 }
+
+let throttledSum = debounceDecoratorNew(sum, 3000);
+throttledSum(123,123,123);
