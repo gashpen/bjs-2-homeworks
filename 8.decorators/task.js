cb26386b7077149150a53b051c19a00e3e63fb0e
gashpen <constantinent@mail.ru>
2023-03-02 14:22:23 +0300

    Homework done

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,50 +1,45 @@
 //Задача № 1
-    function cachingDecoratorNew(func) {
-        let cache = [];
-        
-        function wrapper(...args) {
-            const hash = md5(args); // получаем правильный хеш c помощью функции md5
-            let objectInCache = cache.find((item) => item === cache[hash]); // ищем элемент, хеш которого равен нашему хешу
-            if (objectInCache) { // если элемент найден
-                console.log("Из кэша: " + cache[hash]); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
-                return "Из кэша: " + cache[hash];
-            }
-        
-            let result = func(...args); // в кеше результата нет — придётся считать
-            cache.push(hash) ; // добавляем элемент с правильной структурой
-            if (cache.length > 5) { 
-              cache = cache.shift()// если слишком много элементов в кеше, надо удалить самый старый (первый) 
-            }
-            console.log("Вычисляем: " + result);
-            return "Вычисляем: " + result;  
-        }
-        return wrapper;
-        }
+function cachingDecoratorNew(func) {
+  let cache = [];
 
-
-        
-
-//Задача № 2
-const sum = (...args) => args.reduce((acc, item) => acc + item, 0);
-
-function debounceDecoratorNew(func, delay) {
-  let isThrottled = false;
-  let allCount = [];
-  let count = 0;
-  function wrapper(...args){
-    if(isThrottled){
-      return;
+  return function(...args) {
+    const hash = md5(args);
+    let objectInCache = cache.find(
+      (objectInCache) => objectInCache.hash === hash)
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.result);
+      return "Из кэша: " + objectInCache.result;
     }
-    const result = func(...args);
-    isThrottled = true;
-    setInterval(() => isThrottled = false, delay);
-    setTimeout(() => func(args), delay);
-    count++;
-    return result;
+    let result = func(...args);
+    cache.push({ hash, result });
+
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
   }
-  wrapper.count = () => count;
-  return wrapper
 }
 
-let throttledSum = debounceDecoratorNew(sum, 3000);
-throttledSum(123,123,123);
+//Задача № 2
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  function wrapper(...args) {
+    if (timeoutId === null) {
+      func(...args);
+      wrapper.count++;
+    }
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      timeoutId = true;
+      func(...args);
+      wrapper.count++;
+    }, delay);
+    wrapper.allCount++;
+  }
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  return wrapper;
+}
